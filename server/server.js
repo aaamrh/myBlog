131aa2e84eb0be6125c880270a67b392bc594063
aaamrh <aaamrh@163.com>
2019-04-12 18:41:15 +0800

    mongoose -> connected mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const DB_URL='mongodb://localhost:27017/marh-blog'
+mongoose.connect(DB_URL)
+mongoose.connection.on('connected', function(){console.log('mongodb 链接成功')})
 
 const app = express();
 
 app.get('/', (req, res)=>{
-  res.send('hello world')
+  res.send('hello world') 
 })
 
 app.listen(9093, function(req, res){
