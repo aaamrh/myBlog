950186876bb2d89f2b8534948d406be2bf47b76a
aaamrh <aaamrh@163.com>
2019-08-08 09:45:54 +0800

    modify Nav -> link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,10 +8,10 @@ class Nav extends Component {
     return (
       <div className="m-headnav">
         <Link className='nav active' to='/navigation'>最新</Link>
-        <Link className='nav' to='/navigation'>前端笔记</Link>
-        <Link className='nav' to='/navigation'>后端笔记</Link>
-        <Link className='nav' to='/navigation'>移动笔记</Link>
-        <Link className='nav' to='/navigation'>个人生活</Link>
+        <Link className='nav del' to='#'>前端笔记</Link>
+        <Link className='nav del' to='#'>后端笔记</Link>
+        <Link className='nav del' to='#'>移动笔记</Link>
+        <Link className='nav del' to='#'>个人生活</Link>
         <Link className='nav' to='/navigation'>私藏的网站</Link>
       </div>
     );
