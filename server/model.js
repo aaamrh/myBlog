faa16ec2147b6fa79d06107fcba05923fa3bb71c
maruihua <aaamrh@163.com>
2019-11-18 08:59:57 +0800

    not important

--- server/model.js
+++ server/model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const DB_URL='mongodb://localhost:27017/maBlog';
-// const DB_URL='mongodb://marh:010933@localhost:27017/maBlog';
+// const DB_URL='mongodb://localhost:27017/maBlog';
+const DB_URL='mongodb://marh:010933@localhost:27017/maBlog';
 
 
 mongoose.connect(DB_URL, {useNewUrlParser: true})
