63fb53b39b5693fb4a18b16497dc847f20ff336d
maruihua <aaamrh@163.com>
2019-11-27 12:20:57 +0800

    qrcode

--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -5,8 +5,11 @@ class Tools extends Component {
   render() {
     return (
       <div className="tools">
-        <div className="wechat"></div>
-        
+        <div className="wechat">
+          <div className='qrcode'>
+            <p>扫码激情交友</p>
+          </div>
+        </div>
       </div>
     );
   }
